9b50e37a37d1080bf7fa9d50477d5c934881c3df
Valentin Pashov <valentinpashov04@gmail.com>
2026-04-03 18:32:31 +0300

    feat: Added basis Express serverwith PostgreSQL connection

--- server/index.js
+++ server/index.js
@@ -18,3 +18,22 @@ const pool = new Pool({
     database: process.env.DB_NAME
 });
 
+app.get('/', (req, res) => {
+    res.send('The Server WorkStation works successfully!');
+});
+
+// Get all users
+app.get('/api/users', async (req, res) => {
+    try {
+        const allUsers = await pool.query('SELECT * FROM users');
+        res.json(allUsers.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Грешка в сървъра');
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`The server is listening on port ${PORT}`);
+});
