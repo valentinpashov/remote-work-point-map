b82465e8a5e8a631efa2eb49fa09ba30be86273c
Valentin Pashov <valentinpashov04@gmail.com>
2026-04-08 14:26:01 +0300

    refactor: Added a catalog page in the app structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home'; 
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Catalog from './pages/Catalog';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex flex-col h-screen w-full font-sans">
+      <div className="flex flex-col h-screen">
         <Navbar />
-        
-        <div className="flex-1 relative z-0">
+        <div className="flex-1 overflow-auto">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<Catalog />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Routes>
